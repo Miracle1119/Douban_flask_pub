355f526fa6d60feb319d326e2d52083642f03f0e
Douban_flask <834591497@qq.com>
2022-06-10 19:37:10 +0800

    first commit

---
+++ test_Cloud.py
@@ -0,0 +1,52 @@
+# -*- coding = utf-8 -*-
+# @Time : 2021-3-14 21:16
+# @Author : Miracle
+# @File : test_Cloud.py
+# @Software : PyCharm
+
+
+import jieba        #分词
+from matplotlib import pyplot as plt    #绘图，数据可视化
+from wordcloud import WordCloud         #词云
+from PIL import Image                   #图片处理（官方认可好用）
+import numpy as np                      #矩阵运算
+import sqlite3                          #数据库
+
+
+con = sqlite3.connect('movie.db')
+cur = con.cursor()
+sql = 'select introduction from movie250'
+data = cur.execute(sql)
+text = ""
+for item in data:
+    text +=  item[0]
+    #print(item[0])
+# print(text)
+cur.close()
+con.close()
+
+#分词
+cut = jieba.cut(text)
+string = ' '.join(cut)
+print(len(string))
+
+
+#绘图
+img = Image.open(r'static/assets/img/tree.jpg')     #r用来使转义字符/失效
+img_array = np.array(img)   #将图片转换为数组
+wc = WordCloud(
+    background_color='white',
+    mask=img_array,
+    font_path="STHUPO.TTF"      #字体所在位置C:\Windows\Fonts
+)
+wc.generate_from_text(string)
+
+#绘制图片
+fig = plt.figure(1)
+plt.imshow(wc)
+plt.axis('off')     #是否显示坐标轴
+
+# plt.show()  #显示生成的词云图片
+
+#输出词云图片到文件
+plt.savefig(r'static/assets/img/WordCloud.jpg',dpi=500)
